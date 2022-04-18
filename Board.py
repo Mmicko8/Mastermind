69c2b61eee0ea42d5909a4fbe5b8f601031ad16c
Michel Matthe <matthe.michel@gmail.com>
2022-04-18 23:35:43 +0200

    almost finished converting board.py to OOP

--- Board.py
+++ Board.py
@@ -8,6 +8,7 @@ from tkinter import (Button,
 
 class Board:
     ALL_COLORS = ("red", "green", "blue", "yellow", "orange", "white")
+
     # NUMBER_OF_CIRCLES = 4
     # MAX_NUMBER_OF_MOVES = 10
 
@@ -34,6 +35,15 @@ class Board:
             self.canvas.itemconfig(self.ovals[current_move][len(guess_combination)], fill=color_name)
             guess_combination.append(color_name)
 
+    def draw_color_buttons(self, current_move, guess_combination):
+        for i in range(len(self.ALL_COLORS)):
+            self.canvas.create_window(self.number_of_circles * 50 + 10, 20 + i * 45, anchor=NW,
+                                      window=Button(self.canvas,
+                                                    command=lambda color_name=self.ALL_COLORS[i]: self.color(
+                                                        color_name, current_move, guess_combination),
+                                                    width=5,
+                                                    text=self.ALL_COLORS[i]))
+
     def create_empty_circles(self):
         """ Returns a matrix containing grey ovals that are correctly initialized
             at their required location."""
@@ -44,12 +54,46 @@ class Board:
 
         for i in range(self.max_number_of_moves):
             for x in range(self.number_of_circles):
-                circle = self.canvas.create_oval(10 + 40 * x, 10 + 40 * i, 40 + 40 * x, 40 + 40 * i, fill='light gray')
+                circle = self.canvas.create_oval(10 + 40 * x, 10 + 40 * i, 40 + 40 * x, 40 + 40 * i,
+                                                 fill='light gray')
                 ovals[i].append(circle)
 
         return ovals
 
-    def draw_board(self, root):
+    def draw_result_labels(self):
+        self.canvas.create_window(50, self.max_number_of_moves * 40 + 30, anchor=NW,
+                                  window=Label(self.canvas, text="Correct position: "))
+
+        matching_position_label = Label(self.canvas)
+        self.canvas.create_window(200, self.max_number_of_moves * 40 + 30, anchor=NW, window=matching_position_label)
+
+        self.canvas.create_window(50, self.max_number_of_moves * 40 + 60, anchor=NW,
+                                  window=Label(self.canvas, text="Correct color: "))
+
+        correct_color_label = Label(self.canvas)
+        self.canvas.create_window(200, self.max_number_of_moves * 40 + 60, anchor=NW, window=correct_color_label)
+
+    def draw_submit_button(self):
+        submit_button = Button(self.canvas, text="Check", command=check_combination)
+        submit_button.configure(width=10, relief=FLAT)
+        self.canvas.create_window(50, self.max_number_of_moves * 40 + 90, anchor=NW, window=submit_button)
+
+    def draw_sublist_button(self):
+        sublist_button = Button(self.canvas, text="Sublist", command=is_sublist)
+        sublist_button.configure(width=10, relief=FLAT)
+        self.canvas.create_window(180, self.max_number_of_moves * 40 + 90, anchor=NW, window=sublist_button)
+
+    def draw_hint_button(self):
+        hint_button = Button(self.canvas, text="Hint", command=hint)
+        hint_button.configure(width=10, relief=FLAT)
+        self.canvas.create_window(50, self.max_number_of_moves * 40 + 130, anchor=NW, window=hint_button)
+
+    def draw_quit_button(self, root):
+        quit_button = Button(self.canvas, text="Quit", command=root.destroy)
+        quit_button.configure(width=10, relief=FLAT)
+        self.canvas.create_window(180, self.max_number_of_moves * 40 + 130, anchor=NW, window=quit_button)
+
+    def draw_board(self, root, current_move, guess_combination):
         """
           Create the graphical user interface.
         """
@@ -57,14 +101,9 @@ class Board:
         # provide the title that will be shown in the header
         root.title("Mastermind")
 
-        # # Indicate which variables of the program will be altered
-        # # ! In order to manipulate variables that are part of the program
-        # # ! but exist outside of the function, you need to indicate that you want
-        # # ! to use and alter these variables by declaring them as "global"
-        # global canvas, ovals, matching_position_label, correct_color_label
-
         # canvas will be the main board used for mastermind
-        canvas = Canvas(root, bg="white", height=self.max_number_of_moves * 40 + 170, width=self.number_of_circles * 50 + 100)
+        canvas = Canvas(root, bg="white", height=self.max_number_of_moves * 40 + 170,
+                        width=self.number_of_circles * 50 + 100)
 
         # Draw the empty circles representing the guesses
         self.ovals = self.create_empty_circles()
@@ -73,56 +112,31 @@ class Board:
         canvas.create_line(self.number_of_circles * 50, 10, self.number_of_circles * 50, 400, width=3, fill="black")
 
         # Draw the color buttons that will be used throughout the game
-        # ! Every available color (listed here as strings in the global variable
-        # ! ALL_COLORS) is represented in the GUI by a button
-        # ! when the user clicks the button the function color(color_name)
-        # ! is called
-        for i in range(len(self.ALL_COLORS)):
-            canvas.create_window(self.number_of_circles * 50 + 10, 20 + i * 45, anchor=NW,
-                                 window=Button(canvas,
-                                               command=lambda color_name=self.ALL_COLORS[i]: self.color(color_name),
-                                               width=5,
-                                               text=self.ALL_COLORS[i]))
+        self.draw_color_buttons(current_move, guess_combination)
 
         # Draw the result labels
         # ! matching_position_label and correct_color_label can be used later on
         # ! to show the user the number of correct positions and colors
         # ! (e.g. matching_position_label["text"] = "2")
-        canvas.create_window(50, self.max_number_of_moves * 40 + 30, anchor=NW,
-                             window=Label(canvas, text="Correct position: "))
-        matching_position_label = Label(canvas)
-        canvas.create_window(200, self.max_number_of_moves * 40 + 30, anchor=NW, window=matching_position_label)
-
-        canvas.create_window(50, self.max_number_of_moves * 40 + 60, anchor=NW,
-                             window=Label(canvas, text="Correct color: "))
-        correct_color_label = Label(canvas)
-        canvas.create_window(200, self.max_number_of_moves * 40 + 60, anchor=NW, window=correct_color_label)
+        self.draw_result_labels()
 
         # Draw the submit button
         # ! when the user clicks the button,
         # ! the function check_combination is called
-        submit_button = Button(canvas, text="Check", command=check_combination)
-        submit_button.configure(width=10, relief=FLAT)
-        canvas.create_window(50, self.max_number_of_moves * 40 + 90, anchor=NW, window=submit_button)
+        self.draw_submit_button()
 
         # Draw the sublist button
         # ! when the user clicks the button,
         # ! the function is_sublist is called
-        sublist_button = Button(canvas, text="Sublist", command=is_sublist)
-        sublist_button.configure(width=10, relief=FLAT)
-        canvas.create_window(180, self.max_number_of_moves * 40 + 90, anchor=NW, window=sublist_button)
+        self.draw_sublist_button()
 
         # Draw the hint button
         # ! when the user clicks the button,
         # ! the function hint is called
-        hint_button = Button(canvas, text="Hint", command=hint)
-        hint_button.configure(width=10, relief=FLAT)
-        canvas.create_window(50, self.max_number_of_moves * 40 + 130, anchor=NW, window=hint_button)
+        self.draw_hint_button()
 
         # Draw the quit button
-        quit_button = Button(canvas, text="Quit", command=root.destroy)
-        quit_button.configure(width=10, relief=FLAT)
-        canvas.create_window(180, self.max_number_of_moves * 40 + 130, anchor=NW, window=quit_button)
+        self.draw_quit_button(root)
 
         # draw the canvas
         canvas.pack()
